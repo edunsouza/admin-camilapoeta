b11b7ba1705ef245e7eb0fc49ac3ff1a29261f08
edunsouza <eduardo.souza.ens@gmail.com>
2023-09-30 13:46:42 -0300

    fixing safari iOS issues: * preventing auto bg/theme color * preventing zoom on focus * preventing scroll on focus

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
+import { useOnMount } from './hooks/useOnMount';
+import { Router } from './routes';
+import { PageHeader, SideMenu } from './components/Page';
+import './styles/overrides.scss';
 import './App.scss';
 
 function App() {
+  useOnMount(() => {
+    const focusOutHandler = () => setTimeout(() => window.scrollTo({
+      // iOS safari "scroll on focus" workaround
+      top: window.scrollY + 1,
+      behavior: 'smooth'
+    }), 100);
+
+    document.addEventListener('focusout', focusOutHandler);
+
+    return () => document.removeEventListener('focusout', focusOutHandler);
+  });
+
   return (
     <>
-      <p>TESTE</p>
+      <PageHeader />
+      <SideMenu />
+      <Router />
     </>
   );
 }
