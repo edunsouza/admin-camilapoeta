fa04cce4447257faeb38d614528f987853cfedc1
edunsouza <eduardo.souza.ens@gmail.com>
2023-11-09 22:47:52 -0300

    creating admin-service worker in service/

---
+++ service/src/index.ts
@@ -0,0 +1,24 @@
+export interface Env {
+	PROD_DB_HOST: string;
+	PROD_DB_PASSWORD: string;
+	PROD_DB_USER: string;
+}
+
+export default {
+	async fetch(
+		request: Request,
+		env: Env,
+		ctx: ExecutionContext
+	): Promise<Response> {
+		return new Response(JSON.stringify({
+			METHOD: request.method,
+			'env.PROD_DB_HOST': env.PROD_DB_HOST,
+			'env.PROD_DB_PASSWORD': env.PROD_DB_PASSWORD,
+			'env.PROD_DB_USER': env.PROD_DB_USER,
+		}), {
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		});
+	},
+};
