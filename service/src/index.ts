58f9ed53552020f21281fa17971ee47c3cb2ac0a
edunsouza <eduardo.souza.ens@gmail.com>
2024-04-01 03:33:53 -0300

    indenting & uncommenting index / commenting deploy-service

--- service/src/index.ts
+++ service/src/index.ts
@@ -7,28 +7,26 @@ import * as customersService from './customers.controller';
 const isDev = (env: WorkerEnv) => env.WORKER_ENV === 'dev';
 
 const getAllowedOrigin = (env: WorkerEnv) => {
-	return isDev(env) ? '*' : ['https://admin.camilapoeta.com'];
+  return isDev(env) ? '*' : ['https://admin.camilapoeta.com'];
 };
 
-// triggering gh action - attempt #2
-
 export default {
-	async fetch(request: Request, env: WorkerEnv, ctx: ExecutionContext) {
-		const app = new Hono<{ Bindings: WorkerEnv }>();
+  async fetch(request: Request, env: WorkerEnv, ctx: ExecutionContext) {
+    const app = new Hono<{ Bindings: WorkerEnv }>();
 
-		// MIDDLEWARES
-		app.use('*', cors({ origin: getAllowedOrigin(env) }));
-		app.use('*', authChecker);
+    // MIDDLEWARES
+    app.use('*', cors({ origin: getAllowedOrigin(env) }));
+    app.use('*', authChecker);
 
-		// CUSTOMERS
-		app.get('/customers', customersService.getAll);
-		app.post('/customers', customersService.create);
-		app.get('/customers/:id', customersService.getById);
-		app.patch('/customers/:id', customersService.updateById);
+    // CUSTOMERS
+    app.get('/customers', customersService.getAll);
+    app.post('/customers', customersService.create);
+    app.get('/customers/:id', customersService.getById);
+    app.patch('/customers/:id', customersService.updateById);
 
-		// FALLBACK
-		app.get('*', customersService.getAll);
+    // FALLBACK
+    app.get('*', customersService.getAll);
 
-		return app.fetch(request, env, ctx);
-	}
+    return app.fetch(request, env, ctx);
+  }
 };
